b20015e8a6b03d8635886863de385945ef9afe28
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-11-20 22:16:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace Algoritmo_Desempenho
+{
+   
+    class Program
+    {
+        static long tamanho = 100000000;
+        static List<int> elementos = new List<int>();
+
+        static void dados()
+        {
+            elementos = new List<int>();
+            for (long i = 0; i < tamanho; i++)
+            {
+                Random aleatorio = new Random();
+                int num = aleatorio.Next((Int32)i, (Int32)tamanho);
+
+                elementos.Add(num);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch tempo = new Stopwatch();
+
+            Console.WriteLine("Iniciando..");
+            Console.WriteLine("Gerando valores...");
+            tempo.Start();
+            dados();//substituir pelo método de ordenação
+            tempo.Stop();
+            Console.WriteLine("Resultado: " + tempo.Elapsed.ToString("mm\\:ss\\.ff"));
+            Console.ReadLine();
+        }
+    }
+}
